3bcd2dd6c772babaff65345200290f836dde9e34
zero-spec <81307290+zero-spec@users.noreply.github.com>
2022-09-13 12:33:00 +0700

    Update config.js

--- command/Edit/config.js
+++ command/Edit/config.js
@@ -1,23 +1,23 @@
 const { modul } = require('../Edit/module');
 const { chalk, fs, proces } = modul;
 
-global.ownerNumber = ["6282399255216@s.whatsapp.net"]
+global.ownerNumber = ["6283177683343@s.whatsapp.net"]
 global.namabotnya = 'Cyber Is Real'
 global.namaownernya = 'Cyber Executed'
 global.packname = '\n\n\n\n\n\n\n\n\n\n© Cyber||+62🇲🇨\nI`m From Indonesia\n\n\n\n\n\n\n\n\n\n'
-global.author = '\n\n\n\n\n\n\n\n\n\nWa : 0823-9925-5216\nYT : Cyber S-C-D-B\n\n\n\n\n\n\n\n\n\n'
+global.author = '\n\n\n\n\n\n\n\n\n\nWa : 0831-7768-3343\nYT : Cyber S-C-D-B\n\n\n\n\n\n\n\n\n\n'
 global.sessionName = 'CyberExe'
 global.lolkey = 'Atakbotz'
 global.dapkey = 'Kirbotz123'
 global.kirkey = 'KirBotz'
-global.email = 'id3026799@gmail.com'
+global.email = 'tesspeee@gmail.com'
 global.group = 'https://bit.ly/38oNU0D'
 global.youtube = 'https://bit.ly/39Ivus6'
 global.website = 'https://bit.ly/3yk3gOo'
 global.github = 'https://bit.ly/3FqVBj8'
 global.noown = 'https://wa.me/6282399255216'
 global.region = 'I`m From Indonesia'
-global.prefa = ['','!','.','#','-','�?']
+global.prefa = ['','!','.','#','-','�?']
 global.api = 
 {
     success: '```Success✅```',
